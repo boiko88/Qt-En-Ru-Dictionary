fe9086dcab9a1653e8a4adeea722436c021e18aa
unknown <you@example.com>
2022-06-19 01:41:04 +0400

    added flags of the countries and added a message box

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QGridLayout, QLineEdit
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QGridLayout, QLineEdit, QMessageBox
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
 from PyQt5.QtGui import QCursor
@@ -25,11 +25,15 @@ rus_dict = []
 
 # Functions 
 
+
+def add_no_data_error():
+    print("Sorry, empty info cannot be added. Please make sure you actually write something before adding it to the dictionary.")
+    QMessageBox.about(window, "Empty Data Input", "Sorry, empty info cannot be added. Please make sure you actually write something before adding it to the dictionary.")
 def add_russian_word():
     actual_new_ru_word = new_rus_entry.text()
     with open ("russian_dictionary.csv", "a", newline="") as data_file:
         if actual_new_ru_word == "":
-            print("Sorry, empty info cannot be added. Please make sure you actually write something.")
+            add_no_data_error()
         else:
             data_file.write(f"{actual_new_ru_word}\n")
             winsound.PlaySound("click.wav", winsound.SND_ASYNC)
@@ -38,7 +42,7 @@ def add_english_word():
     actual_new_eng_word = new_eng_entry.text()
     with open ("english_dictionary.csv", "a", newline="") as data_file:
         if actual_new_eng_word == "":
-            print("Sorry, empty info cannot be added. Please make sure you actually write something.")
+            add_no_data_error()
         else:
             data_file.write(f"{actual_new_eng_word}\n")
             winsound.PlaySound("click.wav", winsound.SND_ASYNC)
@@ -64,13 +68,25 @@ def ask_random_rus_word():
 
 
         
-# Display Dictionary picture
+# Display Pictures
 
 
-image = QPixmap("dictionary_1.png")
+dict_image = QPixmap("dictionary_1.png")
 dict = QLabel()
-dict.setPixmap(image)
+dict.setPixmap(dict_image)
 dict.setAlignment(QtCore.Qt.AlignCenter)
+
+english_flag = QPixmap("UK_Flag.png")
+uk_flag = QLabel()
+uk_flag.setPixmap(english_flag)
+uk_flag.setAlignment(QtCore.Qt.AlignCenter)
+
+russian_flag = QPixmap("RU_Flag.png")
+ru_flag = QLabel()
+ru_flag.setPixmap(russian_flag)
+ru_flag.setAlignment(QtCore.Qt.AlignCenter)
+
+
 
 
 # Buttons
@@ -108,26 +124,32 @@ random_rus_button.setStyleSheet(
 # Entry
 
 new_eng_entry = QLineEdit()
-new_eng_entry.setPlaceholderText("Type an English word here")
+new_eng_entry.setPlaceholderText("Type an English word")
 new_eng_entry.setStyleSheet(
         hover_CSS
     )
 
 new_rus_entry = QLineEdit()
-new_rus_entry.setPlaceholderText("Type a Russia word here")
+new_rus_entry.setPlaceholderText("Type a Russia word")
 new_rus_entry.setStyleSheet(
         hover_CSS
     )
+
+# Grid all the elements 
+
 grid.addWidget(dict, 0, 0)
 
-grid.addWidget(eng_button, 2, 0)
-grid.addWidget(new_eng_entry, 1,0)
+grid.addWidget(uk_flag, 1, 0 )
+grid.addWidget(ru_flag, 1, 1 )
 
-grid.addWidget(rus_button, 2, 1)
-grid.addWidget(new_rus_entry, 1, 1)
+grid.addWidget(eng_button, 3, 0)
+grid.addWidget(new_eng_entry, 2,0)
 
-grid.addWidget(random_eng_button, 3, 0)
-grid.addWidget(random_rus_button, 3, 1)
+grid.addWidget(rus_button, 3, 1)
+grid.addWidget(new_rus_entry, 2, 1)
+
+grid.addWidget(random_eng_button, 4, 0)
+grid.addWidget(random_rus_button, 4, 1)
 
 
 window.setLayout(grid)
